09c2a2a851bf01fb6c6ff574075c7ad2218ffec7
Ivan <xmillenium.ij@gmail.com>
2017-11-19 13:14:02 +0700

    LAST

---
+++ GDrive/stackunit.c
@@ -0,0 +1,50 @@
+/* File : stackt.c */
+/* implementasi stack yang diimplementasi dengan tabel kontigu dan ukuran sama */
+/* TOP adalah alamat elemen puncak */
+/* Implementasi dalam bahasa C dengan alokasi statik */
+
+#include "boolean.h"
+#include "stackunit.h"
+#include "listlinierunit.h"
+#include "point.h"
+#include <stdio.h>
+
+/* ************ Prototype ************ */
+/* *** Konstruktor/Kreator *** */
+void CreateEmptyS (Stack *S)
+/* I.S. sembarang; */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* jadi indeksnya antara 1.. MaxEl+1 karena 0 tidak dipakai */
+/* Ciri stack kosong : TOP bernilai Nil */
+{
+    Top(*S)=Nils;
+}
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmptyS (Stack S)
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
+{
+    return(Top(S)==Nils);
+}
+boolean IsFullS (Stack S)
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+{
+    return(Top(S)==MaxEl);
+}
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push (Stack * S, infotype X)
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+{
+    Top(*S)=Top(*S)+1;
+    InfoTop(*S)=X;
+}
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop (Stack * S, infotype* X)
+/* Menghapus X dari Stack S. */
+/* I.S. S  tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+{
+    *X = InfoTop(*S);
+    Top(*S)--;
+}
